351618996bb6930d6e9e3b8a2855a2082830cd2f
David Chen <qydchen@gmail.com>
2024-01-19 09:02:09 -0800

    fix build after upgrades

--- apps/personal_website/next.config.js
+++ apps/personal_website/next.config.js
@@ -1,5 +1,6 @@
-const withTM = require("next-transpile-modules")(["utils"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+};
