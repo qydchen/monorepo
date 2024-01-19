c8902c094db628e2678d3e4f33fe23b3956ed93e
David Chen <qydchen@gmail.com>
2024-01-19 08:40:18 -0800

    clean Link

--- apps/personal_website/components/layout.tsx
+++ apps/personal_website/components/layout.tsx
@@ -49,7 +49,6 @@ export default function Layout({
         ) : (
           <>
             <Link href="/">
-              <a>
                 <Image
                   priority
                   src="/images/profile.jpg"
@@ -58,11 +57,10 @@ export default function Layout({
                   width={108}
                   alt={name}
                 />
-              </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
               </Link>
             </h2>
           </>
@@ -72,7 +70,7 @@ export default function Layout({
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
-            <a>← Back to home</a>
+            ← Back to home
           </Link>
         </div>
       )}
